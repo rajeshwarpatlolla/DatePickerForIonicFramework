8989e47dea263167d6e52e96ff3ba92d07e9b704
rajeshwar.patlolla <rajeshwar.patlolla@fissionlabs.in>
2015-06-13 13:23:31 +0530

    change for new version of ionic-datepicker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,11 @@ angular.module('starter.controllers', [])
   .controller('DatePickerCtrl', function ($scope, $ionicPopup) {
     $scope.currentDate = new Date();
     $scope.pastDate = new Date(1521199764000);
+
+    $scope.datePickerCallback = function (x) {
+      console.log('in controller callback', x, typeof(x))
+    };
+
   })
 
   .controller('ContactCtrl', function ($scope) {

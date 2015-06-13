c01c8a4f226e9f39f4182d9184ff1a361878bde6
rajeshwar.patlolla <rajeshwar.patlolla@fissionlabs.in>
2015-06-13 13:57:13 +0530

    ionic-datepicker v0.3.0 installed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,15 @@ angular.module('starter.controllers', [])
     $scope.currentDate = new Date();
     $scope.pastDate = new Date(1521199764000);
 
-    $scope.datePickerCallback = function (x) {
-      console.log('in controller callback', x, typeof(x))
+    $scope.datePickerCallback = function (val) {
+      console.log('ionic-datepicker callback');
+
+      if (typeof(val) === 'undefined') {
+        console.log('No date selected');
+      } else {
+        console.log('Selected date is : ', val)
+      }
+
     };
 
   })

3b10e1ed8e80b910cd47fe66bc9c2add6af06806
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-07-03 12:19:11 +0530

    updated to ionic-timepicker v0.4.0

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,7 @@ angular.module('starter.controllers', [])
   .controller('DatePickerCtrl', function ($scope, $ionicPopup) {
     $scope.currentDate = new Date();
     $scope.pastDate = new Date(1521199764000);
+    $scope.title = 'Custom Title';
 
     $scope.datePickerCallback = function (val) {
       console.log('ionic-datepicker callback');

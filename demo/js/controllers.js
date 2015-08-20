d79ab21005ee763a7b8c6a369720be6cf263e1c2
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-08-20 10:34:19 +0530

    ionic-datepicker v0.8.0 installed

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -4,36 +4,70 @@ angular.module('starter.controllers', [])
 
     var disabledDates = [
       new Date(1437719836326),
-      new Date(),
       new Date(2015, 7, 10), //months are 0-based, this is August, 10th!
       new Date('Wednesday, August 12, 2015'), //Works with any valid Date formats like long format
       new Date("08-14-2015"), //Short format
       new Date(1439676000000) //UNIX format
     ];
     var monthList = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    $scope.datepickerObject = {};
+    $scope.datepickerObject.inputDate = new Date();
 
-    $scope.datepickerObject = {
-      titleLabel: 'Title',
-      todayLabel: 'Today',
-      closeLabel: 'Close',
-      setLabel: 'Set',
-      errorMsgLabel : 'Please select time.',
-      setButtonType : 'button-assertive',
-      inputDate: new Date(),
-      mondayFirst: true,
-      disabledDates:disabledDates,
-      monthList:monthList,
-      from: new Date(2015, 7, 2),
-      to: new Date(2015, 7, 29),
-      callback: function (val) {
-        datePickerCallback(val);
+    $scope.datepickerObjectModal = {
+      titleLabel: 'Ionic-Datepicker', //Optional
+      todayLabel: 'Today', //Optional
+      closeLabel: 'Close', //Optional
+      setLabel: 'Set', //Optional
+      errorMsgLabel : 'Please select time.', //Optional
+      setButtonType : 'button-assertive', //Optional
+      modalHeaderColor:'bar-positive', //Optional
+      modalFooterColor:'bar-positive', //Optional
+      templateType:'modal', //Optional
+      inputDate: $scope.datepickerObject.inputDate, //Optional
+      mondayFirst: true, //Optional
+      disabledDates:disabledDates, //Optional
+      monthList:monthList, //Optional
+      from: new Date(2015, 5, 1), //Optional
+      to: new Date(2016, 7, 1), //Optional
+      callback: function (val) { //Optional
+        datePickerCallbackModal(val);
+      }
+    };
+    $scope.datepickerObjectPopup = {
+      titleLabel: 'Ionic-Datepicker', //Optional
+      todayLabel: 'Today', //Optional
+      closeLabel: 'Close', //Optional
+      setLabel: 'Set', //Optional
+      errorMsgLabel : 'Please select time.', //Optional
+      setButtonType : 'button-assertive', //Optional
+      modalHeaderColor:'bar-positive', //Optional
+      modalFooterColor:'bar-positive', //Optional
+      templateType:'popup', //Optional
+      inputDate: $scope.datepickerObject.inputDate, //Optional
+      mondayFirst: true, //Optional
+      disabledDates:disabledDates, //Optional
+      monthList:monthList, //Optional
+      from: new Date(2015, 5, 1), //Optional
+      to: new Date(2016, 7, 1), //Optional
+      callback: function (val) { //Optional
+        datePickerCallbackPopup(val);
       }
     };
 
-    var datePickerCallback = function (val) {
+    var datePickerCallbackModal = function (val) {
       if (typeof(val) === 'undefined') {
         console.log('No date selected');
       } else {
+        $scope.datepickerObjectModal.inputDate = val;
+        console.log('Selected date is : ', val)
+      }
+    };
+
+    var datePickerCallbackPopup = function (val) {
+      if (typeof(val) === 'undefined') {
+        console.log('No date selected');
+      } else {
+        $scope.datepickerObjectPopup.inputDate = val;
         console.log('Selected date is : ', val)
       }
     };

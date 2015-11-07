00d04b598e70681058224345aea1b8e685915c93
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-11-07 16:17:58 +0530

    changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,28 +7,28 @@ angular.module('starter.controllers', [])
       new Date(2015, 7, 10), //months are 0-based, this is August, 10th!
       new Date('Wednesday, August 12, 2015'), //Works with any valid Date formats like long format
       new Date("08-14-2015"), //Short format
-      new Date(1439676000000) //UNIX format
+      new Date(1439676000000), //UNIX format
+      new Date(),
+      new Date(2015, 10, 8)
     ];
     var monthList = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
     $scope.datepickerObject = {};
     $scope.datepickerObject.inputDate = new Date();
 
     $scope.datepickerObjectModal = {
-      titleLabel: 'Ionic-Datepicker', //Optional
       todayLabel: 'Today', //Optional
       closeLabel: 'Close', //Optional
       setLabel: 'Set', //Optional
-      errorMsgLabel : 'Please select time.', //Optional
-      setButtonType : 'button-assertive', //Optional
-      modalHeaderColor:'bar-positive', //Optional
-      modalFooterColor:'bar-positive', //Optional
-      templateType:'modal', //Optional
+      setButtonType: 'button-assertive', //Optional
+      modalHeaderColor: 'bar-positive', //Optional
+      modalFooterColor: 'bar-positive', //Optional
+      templateType: 'modal', //Optional
       inputDate: $scope.datepickerObject.inputDate, //Optional
       mondayFirst: true, //Optional
-      disabledDates:disabledDates, //Optional
-      monthList:monthList, //Optional
-      from: new Date(2012, 5, 1), //Optional
-      to: new Date(2016, 7, 1), //Optional
+      disabledDates: disabledDates, //Optional
+      monthList: monthList, //Optional
+      from: new Date(2015, 9, 31), //Optional
+      to: new Date(2015, 10, 21), //Optional
       callback: function (val) { //Optional
         datePickerCallbackModal(val);
       }
@@ -38,17 +38,16 @@ angular.module('starter.controllers', [])
       todayLabel: 'Today', //Optional
       closeLabel: 'Close', //Optional
       setLabel: 'Set', //Optional
-      errorMsgLabel : 'Please select time.', //Optional
-      setButtonType : 'button-assertive', //Optional
-      modalHeaderColor:'bar-positive', //Optional
-      modalFooterColor:'bar-positive', //Optional
-      templateType:'popup', //Optional
+      errorMsgLabel: 'Please select time.', //Optional
+      setButtonType: 'button-assertive', //Optional
+      modalHeaderColor: 'bar-positive', //Optional
+      modalFooterColor: 'bar-positive', //Optional
+      templateType: 'popup', //Optional
       inputDate: $scope.datepickerObject.inputDate, //Optional
-      mondayFirst: true, //Optional
-      disabledDates:disabledDates, //Optional
-      monthList:monthList, //Optional
-      from: new Date(2014, 5, 1), //Optional
-      to: new Date(2016, 7, 1), //Optional
+      mondayFirst: false, //Optional
+      disabledDates: disabledDates, //Optional
+      monthList: monthList, //Optional
+      from: new Date(2015, 9, 1), //Optional
       callback: function (val) { //Optional
         datePickerCallbackPopup(val);
       }
